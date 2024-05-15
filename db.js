dfbc44a31db04a017759376b90cf51e2c16a720f
Ahmed Alqarni <87015348+AhmedAlqarniGitHub@users.noreply.github.com>
2024-05-15 22:32:52 +0300

    Update db.js db connection removed

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 // Replace 'yourDatabaseName' with the actual name of your local MongoDB database
 //const DB_CONNECTION_STRING = 'mongodb://localhost:27017/citra';
-const DB_CONNECTION_STRING='mongodb+srv://ahmedalg4321:citra321@cluster0.u2aiu58.mongodb.net/citra?retryWrites=true&w=majority'
+const DB_CONNECTION_STRING=''
 
 
 mongoose.connect(DB_CONNECTION_STRING, {
